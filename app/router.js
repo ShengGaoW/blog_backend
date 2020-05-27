8e42daa2c99a80169025a04d7e457e4ad0bc15a0
shenggao <527518190@qq.com>
2020-05-27 22:35:53 +0800

    update router

--- app/router.js
+++ app/router.js
@@ -2,4 +2,5 @@ module.exports = app => {
     const { router, controller } = app;
     router.get('/home', controller.home.index);
     router.get('/cube', controller.home.index);
+    router.get('/', controller.home.index);
   };
